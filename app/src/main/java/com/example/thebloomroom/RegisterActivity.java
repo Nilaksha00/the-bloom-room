e42b3948c8e3d45d9bf77fe011cf11a5ba8ff7cf
Nilaksha00 <nilakshasperera@gmail.com>
2023-12-15 16:03:46 +0530

    Implement login and register user interfaces

---
+++ app/src/main/java/com/example/thebloomroom/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.thebloomroom;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView loginTextView = findViewById(R.id.login_textview);
+
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
